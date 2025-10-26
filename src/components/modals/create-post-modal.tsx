f112bcf1b3846e7407dbbbef7e25a18bac346210
Royweru <royezi17@gmail.com>
2025-10-26 20:23:34 +0300

    Added a use client to the create post modal

--- src/components/modals/create-post-modal.tsx
+++ src/components/modals/create-post-modal.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/dialog';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs';
