8910685e128395065a851e564aeee6dc2b0c00f1
Royweru <royezi17@gmail.com>
2026-01-06 20:43:04 +0300

    Made a change in the homepage and imported the useAuth to pass it to the navbar to find out whether a user is authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-
+'use client';
 import { CTASection } from '@/components/cta-section';
 import { FeaturesSection } from '@/components/features-section';
 import { Footer } from '@/components/footer';
 import { HeroSection } from '@/components/hero-section';
 import { Navbar } from '@/components/navbar';
+import { useAuth } from '@/providers/auth-provider';
 import React from 'react';
 
 
 function Home() {
-  // In a real Home, you'd determine this state from auth cookies/context
-  const isLoggedIn = false; // Change to true to test logged-in view
-
+   const { isAuthenticated:isLoggedIn} = useAuth()
   return (
     <div className="min-h-screen font-sans antialiased overflow-x-hidden bg-white">
       <Navbar isLoggedIn={isLoggedIn} />
